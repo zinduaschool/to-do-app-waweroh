91c51d8a845149983e7e74753fab9af8b360682c
waweroh <scadmsez@gmail.com>
2023-04-20 17:03:07 +0300

    update todo app

--- to-do-app/src/App.jsx
+++ to-do-app/src/App.jsx
@@ -1,70 +1,74 @@
 import { useState } from 'react'
-import Task from './Task'
 
 function App() {
-  const [todo, setTodo] = useState([
-
-    {
-      id : 1,
-      title: 'read',
-      completed : false
-    },
-
-    {
-      id : 2,
-      title: 'jump',
-      completed : true
-    }
+  const [todos, setTodo] = useState([
   ])
 
-  const [newTask , setNewTask] = useState([]);
-    
-  //handleadding
-  
-  const handleRemove = (title) => {
-    setTodo(todo.filter(todo => todo.id !== title));
-    
-  
-  };
+  const addTodo = (title)=>{
+    const newTodo = [...todos, {id: todos.length+1, title: title, completed: false}]
+    setTodo(newTodo)
+  }
 
-  const handleChange = (event) => {
-    setNewTask(event.target.value)};
+  const updateTodo = (index)=>{
+    const allTodo = [...todos]
+    allTodo[index].completed = true
+    setTodo(allTodo)
+  }
 
-  // const completeTask = (id) => {
-  //   setTodo(todo.map(todoItem) => {
-  //     if (todoItem.id === id) {
-  //       return{ ...todoItem, completed : true};
-  //     } else {
-  //       return todoItem;
-
-  //     }
-  //   }));
-  // };
-
+  const deleteTodo = (index)=>{
+    const delTodo = [...todos]
+    delTodo.splice(index, 1)
+    setTodo(delTodo)
+  }
 
   return (
-    <div>
-      <div>
-        <input onChange={handleChange} value={newTask}></input>
-        {/* handleAdd and handleRemove */}
-        
-      </div>
-
-      <div>
-        {todo.map((newTask) =>{
+    
+    <div className='bg-indigo-400'>
+      <h1 className='text-2xl font-bold text-gray-700 flex justify-center mb-12'>Todo App</h1>
+      <div className=' '>
+        <TodoForm addTodo={addTodo}/>
+        {todos.map((todo)=>{
           return (
-            <Task />
-          );
+            <Todo key={todo.id} todo={todo} updateTodo={updateTodo} deleteTodo={deleteTodo}/>
+          )
         })}
-
-        
-            
-
-          
-        
-      </div> 
+      </div>
     </div>
     
+    
+  )
+}
+
+const Todo = ({todo, updateTodo, deleteTodo})=>{
+  return (
+    <div className='flex justify-center mb-1' style={{
+      textDecoration: todo.completed ? 'line-through' : ''
+    }}>
+      <h1 className='text-gray-800 px-2'>{todo.title}</h1>
+      <button className='bg-blue-600 border-2 rounded-md px-1 py-1 border-gray-400 text-white mr-1' onClick={()=> updateTodo(todo.id-1)}>Complete</button>
+      <button className='bg-blue-600 border-2 rounded-md px-1 py-1 border-gray-400 text-white' onClick={()=> deleteTodo(todo.id-1) }>Delete</button>
+    </div>
+  )
+}
+
+const TodoForm = ({addTodo})=>{
+  const [value, setValue] = useState('')
+  const handleChange = (e)=>{
+    setValue(e.target.value)
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    if(!value) return
+    addTodo(value)
+    setValue('')
+  }
+  return (
+    
+    <form className='flex justify-center mb-10' onSubmit={handleSubmit}>
+      <input className=' max-w-full mr-2 border-2 border-gray-400 rounded-md px-1 py-1' type="text" placeholder="add todo" value={value} onChange={handleChange}/>
+      <button className=' bg-blue-600 border-2 rounded-md px-1 py-1 border-gray-400 text-white text-lg-20px' onClick={()=> addTodo(Todo.id + 1)}> Add </button>
+    </form>
+   
   )
 }
 
